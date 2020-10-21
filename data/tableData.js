c88ed85c3471e33b61a10372ea7f8c3ac11b59d8
Violet <70643274+violettaval@users.noreply.github.com>
2020-10-21 11:21:21 -0500

    Add files via upload

---
+++ data/tableData.js
@@ -0,0 +1,10 @@
+var tableArray = [
+  {
+    customerName: "Ahmed",
+    customerEmail: "ahmed@example.com",
+    customerID: "afhaque89",
+    phoneNumber: "000-000-0000"
+  }
+];
+
+module.exports = tableArray;
---
+++ data/waitinglistData.js
@@ -0,0 +1,9 @@
+var waitingArray = [
+  {
+    customerName: "Saima",
+    customerEmail: "saima@example.com",
+    phoneNumber: "000-000-0000",
+    customerID: "saimaCool"
+  }
+];
+module.exports = waitingArray;
